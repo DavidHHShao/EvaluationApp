12658ccd0e76944f406d41eb385f04546022b0a1
Michael Tu <tuzhucheng@outlook.com>
2015-04-23 16:12:15 -0400

    frontend init

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, send_from_directory
+app = Flask(__name__, static_url_path='')
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/')
+def index():
+    return send_from_directory('static', 'index.html')
+
+
+@app.route('/js/<path:path>')
+def send_js(path):
+    return send_from_directory('static/js', path)
+
+
+@app.route('/css/<path:path>')
+def send_css(path):
+    return send_from_directory('static/css', path)
+
 
 if __name__ == "__main__":
     app.run()
